9cc0fa9f519c3e7ee7e5ad97d9f649142bc23d3e
Ernie Munoz <emunoz@fairwaytech.com>
2017-05-16 15:03:34 -0700

    Adding a method to OpportunityService to fetch the current opportunity/attempt number

--- student-library/src/main/java/tds/student/services/abstractions/IOpportunityService.java
+++ student-library/src/main/java/tds/student/services/abstractions/IOpportunityService.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Educational Online Test Delivery System 
  * Copyright (c) 2014 American Institutes for Research
- *     
+ *
  * Distributed under the AIR Open Source License, Version 1.0 
  * See accompanying file AIR-License-1_0.txt or at
  * http://www.smarterapp.org/documents/American_Institutes_for_Research_Open_Source_Software_License.pdf
@@ -9,9 +9,11 @@
 package tds.student.services.abstractions;
 
 import java.util.List;
+import java.util.UUID;
 
 import TDS.Shared.Browser.BrowserInfo;
 import TDS.Shared.Exceptions.ReturnStatusException;
+
 import tds.student.services.data.ApprovalInfo;
 import tds.student.sql.data.OpportunityInfo;
 import tds.student.sql.data.OpportunityInstance;
@@ -26,29 +28,35 @@ import tds.student.sql.data.Testee;
 
 /**
  * @author temp_rreddy
- * 
  */
-public interface IOpportunityService
-{
-  List<TestSelection> getEligibleTests (Testee testee, TestSession session, String grade, BrowserInfo browserInfo) throws ReturnStatusException;
+public interface IOpportunityService {
+  List<TestSelection> getEligibleTests(Testee testee, TestSession session, String grade, BrowserInfo browserInfo) throws ReturnStatusException;
 
-  OpportunityInfo openTest (Testee testee, TestSession session, String testKey) throws ReturnStatusException;
+  OpportunityInfo openTest(Testee testee, TestSession session, String testKey) throws ReturnStatusException;
 
-  OpportunityStatus getStatus (OpportunityInstance oppInstance) throws ReturnStatusException;
+  OpportunityStatus getStatus(OpportunityInstance oppInstance) throws ReturnStatusException;
 
-  boolean setStatus (OpportunityInstance oppInstance, OpportunityStatusChange statusChange) throws ReturnStatusException;
+  boolean setStatus(OpportunityInstance oppInstance, OpportunityStatusChange statusChange) throws ReturnStatusException;
 
-  ApprovalInfo checkTestApproval (OpportunityInstance oppInstance) throws ReturnStatusException;
+  ApprovalInfo checkTestApproval(OpportunityInstance oppInstance) throws ReturnStatusException;
 
-  ApprovalInfo checkSegmentApproval (OpportunityInstance oppInstance) throws ReturnStatusException;
+  ApprovalInfo checkSegmentApproval(OpportunityInstance oppInstance) throws ReturnStatusException;
 
-  void denyApproval (OpportunityInstance oppInstance) throws ReturnStatusException;
+  void denyApproval(OpportunityInstance oppInstance) throws ReturnStatusException;
 
-  TestConfig startTest (OpportunityInstance oppInstance, String testKey, List<String> formKeys) throws ReturnStatusException;
+  TestConfig startTest(OpportunityInstance oppInstance, String testKey, List<String> formKeys) throws ReturnStatusException;
 
-  OpportunitySegments getSegments (OpportunityInstance oppInstance, boolean validate) throws ReturnStatusException;
+  OpportunitySegments getSegments(OpportunityInstance oppInstance, boolean validate) throws ReturnStatusException;
 
-  void waitForSegment (OpportunityInstance oppInstance, int segmentPosition, TestSegmentApproval segmentApproval) throws ReturnStatusException;
+  void waitForSegment(OpportunityInstance oppInstance, int segmentPosition, TestSegmentApproval segmentApproval) throws ReturnStatusException;
 
-  void exitSegment (OpportunityInstance oppInstance, int segmentPosition) throws ReturnStatusException;
+  void exitSegment(OpportunityInstance oppInstance, int segmentPosition) throws ReturnStatusException;
+
+  /**
+   * Returns the current attempt number of the exam
+   *
+   * @param opportunityInstance The object containing exam/opportunity metadata
+   * @return The current attempt number of the exam
+   */
+  int getAttemptNumber(final OpportunityInstance opportunityInstance) throws ReturnStatusException;
 }
